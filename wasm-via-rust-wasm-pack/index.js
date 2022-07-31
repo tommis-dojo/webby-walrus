5c4c9f5b479d7f9e24b30a6de451cc4300f0a180
Thomas Stegmaier <thosteg@gmail.com>
2022-07-31 10:29:25 +0200

    Static wasm variable modified on button click

--- wasm-via-rust-wasm-pack/index.js
+++ wasm-via-rust-wasm-pack/index.js
@@ -4,12 +4,18 @@ import init from "./pkg/hello_world.js";
 
 const runWasm = async () => {
   // Instantiate our wasm module
-  const helloWorld = await init("./pkg/hello_world_bg.wasm");
+  const wsm = await init("./pkg/hello_world_bg.wasm");
 
   // Call the Add function export from wasm, save the result
-  const addResult = helloWorld.add(24, 24);
+  const addResult = wsm.add(24, 24);
 
-  // Set the result onto the body
-  document.body.textContent = `Hello World! addResult: ${addResult}`;
+  // Read the result of a simple function
+  document.getElementById("simple-get").textContent = `addResult: ${addResult}`;
+  
+  // Pressing "inc" will increment number and update the item
+  document.getElementById("inc").onclick = function() {
+    wsm.set(wsm.get() + 1);
+    document.getElementById("inc-val").textContent = `get: ${wsm.get()}`;
+  };
 };
 runWasm();
--- wasm-via-rust-wasm-pack/src/lib.rs
+++ wasm-via-rust-wasm-pack/src/lib.rs
@@ -9,3 +9,18 @@ use wasm_bindgen::prelude::*;
 pub fn add(a: i32, b: i32) -> i32 {
   return a + b;
 }
+
+// Using a static mutable (global) variable only because we need a global state
+// for webassembly
+//
+static mut VAL : i32 = 0;
+
+#[wasm_bindgen]
+pub fn set(value: i32) {
+    unsafe { VAL = value; }
+}
+
+#[wasm_bindgen]
+pub fn get() -> i32 {
+    unsafe { VAL }
+}
